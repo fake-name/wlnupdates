3b924219c4eb13fe0f508f73702ab9bfdb886055
Fake-Name <something@fake-url.com>
2015-04-17 16:30:03 -0700

    Make non-debug mode no longer print all it's queries.

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 
-from settings import DATABASE_IP, DATABASE_DB_NAME, DATABASE_USER, DATABASE_PASS
+from settings import DATABASE_IP, DATABASE_DB_NAME, DATABASE_USER, DATABASE_PASS, SECRET_KEY
 
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -8,10 +8,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 SQLALCHEMY_DATABASE_URI = 'postgresql://{user}:{passwd}@{host}:5432/{database}'.format(user=DATABASE_USER, passwd=DATABASE_PASS, host=DATABASE_IP, database=DATABASE_DB_NAME)
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 
-SQLALCHEMY_ECHO = True
+import sys
+if len(sys.argv) > 1 and "debug" in sys.argv:
+	SQLALCHEMY_ECHO = True
 
 CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
 
 COVER_DIR_BASE = os.path.join(basedir, "./covers/")
 
