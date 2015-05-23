df99396edfe86e1822d4ac27897f385240cfa406
Fake-Name <something@fake-url.com>
2015-05-23 16:38:59 -0700

    JS file for spin-control interface for chapter/volume progress inputs.

--- app/sub_views/watched_view.py
+++ app/sub_views/watched_view.py
@@ -30,13 +30,15 @@ def renderUserLists():
 		series = watch.series_row
 		latest = get_latest_release(series)
 
+		# build easier-to-use dicts we can pass the template,
+		# and calculate a aggregate progress number that works for
+		# direct comparisons across chapter/volume releases
 		avail = {}
 		prog  = {}
 		prog['vol']  = watch.volume   if watch and watch.volume    != None else -1
 		prog['chp']  = watch.chapter  if watch and watch.chapter   != None else -1
 		avail['vol'] = latest.volume  if latest and latest.volume  != None else -1
 		avail['chp'] = latest.chapter if latest and latest.chapter != None else -1
-
 
 		prog['agg']  = 0
 		if prog['vol'] > 0:
@@ -50,10 +52,6 @@ def renderUserLists():
 		if avail['chp'] > 0:
 			avail['agg'] += avail['chp']
 
-		# print(prog)
-		if latest:
-			print(avail)
-			print(latest.volume, latest.chapter)
 		data.append((series, prog, avail))
 
 	return render_template(
