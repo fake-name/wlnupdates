16018b379464d9402d629c1cae69e3df44ccd884
Fake-Name <something@fake-url.com>
2015-05-24 20:41:49 -0700

    Script to upgrade db in one step.

---
+++ upgrade_db.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+# Initialize/Reinitialize the database for the app.
+
+# Note: You need pg_trgm and citext extensions installed for this to work.
+# On ubuntu: sudo apt-get install postgresql-contrib
+# then CREATE EXTENSION citext; CREATE EXTENSION pg_trgm;
+
+set -e
+
+python db_migrate.py db migrate
+python db_migrate.py db upgrade
+python db_migrate.py install_triggers
+python db_migrate.py install_tgm_idx
