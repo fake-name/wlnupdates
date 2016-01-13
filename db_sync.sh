4f438b3a08a15ca370f25e1b3d93d40cc3317445
Fake-Name <something@fake-url.com>
2016-01-12 19:21:18 -0800

    Fix database backup script so it actually installs properly.

--- db_sync.sh
+++ db_sync.sh
@@ -6,4 +6,4 @@ if [ "$(id -u)" != "0" ]; then
 fi
 
 sudo -H -u herp -- bash -c "ssh client@ks1 'sudo -u postgres pg_dump --clean -d wlndb | xz' | pv -cN Db-Fetch-Progress > db_dump_$(date +%Y-%m-%d).sql.xz"
-sudo -H -u postgres -- bash -c "xz -d db_dump_$(date +%Y-%m-%d).sql.xz -c | pv -c | psql -d wlndb"
+sudo -H -u postgres -- env PATH=$PATH  bash -c "xz -d db_dump_$(date +%Y-%m-%d).sql.xz -c | pv -c | psql -d wlndb"
