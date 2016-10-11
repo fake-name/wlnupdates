952a80637b79bc31793f67e3ae232cd3684e03fc
Fake-Name <something@fake-url.com>
2016-10-10 22:04:50 -0700

    Search filter stuff.

--- app/sub_views/search.py
+++ app/sub_views/search.py
@@ -17,6 +17,7 @@ from sqlalchemy.sql.expression import select, desc
 from app import db
 from app import app
 import collections
+import json
 
 def title_search(searchterm, page=1):
 	searchtermclean = bleach.clean(searchterm, strip=True)
@@ -83,16 +84,6 @@ def title_search(searchterm, page=1):
 
 
 
-def render_search_page():
-	print("Render search page call!")
-	results = CommonTags.query                 \
-		.filter(CommonTags.tag_instances > 25) \
-		.order_by(CommonTags.tag)              \
-		.all()
-
-	return render_template('advanced-search.html',
-		available_tags = results
-		)
 
 def execute_search():
 	# Flatten the passed dicts.
@@ -107,13 +98,21 @@ def execute_search():
 	if 'title' in search:
 		return title_search(search['title'])
 	else:
-		return render_search_page()
+		return render_search_page(search)
 
 def do_advanced_search(params):
 	print("Params:")
 	print(params)
 	print()
-	q = Series.query
+
+	# Join on the aggregate functions for sub-chapters.
+	rdte = func.max(Releases.published).label('reldate')
+	rcnt = func.count(Series.releases).label('ccount')
+
+	q = db.session.query(Series, rdte, rcnt).group_by(Series.id)
+
+	q = q.join(Releases)
+	q = q.filter(Releases.series == Series.id)
 
 	if 'tag-category' in params:
 		for text, mode in params['tag-category'].items():
@@ -131,15 +130,14 @@ def do_advanced_search(params):
 			minc, maxc = params['chapter-limits']
 			minc = int(minc)
 			maxc = int(maxc)
-			subq = Releases.query.filter(Releases.series == Series.id).count()
 			if minc > 0:
-				q = q.filter(subq >= minc)
+				q = q.having(rcnt >= minc)
 			if maxc > 0:
-				q = q.filter(subq <= maxc)
+				q = q.having(rcnt <= maxc)
 
 	type_map = {
-		'Translated'                : 'oel',
-		'Original English Language' : 'translated',
+		'Translated'                : 'translated',
+		'Original English Language' : 'oel',
 	}
 
 	if 'series-type' in params:
@@ -155,43 +153,84 @@ def do_advanced_search(params):
 		if ops:
 			q = q.filter(and_(*ops))
 
-	if "sort-mode" in params and False:
+	if "sort-mode" in params:
 
 		if params['sort-mode'] == "update":
-			subq = Releases.query.filter(Releases.series == Series.id).max(Releases.published).one()
-			q = q.order_by(desc(subq))
+			q = q.order_by(desc(rdte))
 		elif params['sort-mode'] == "chapter-count":
-			subq = func.count(Releases.query.filter(Releases.series == Series.id))
-			q = q.order_by(desc(subq))
-
+			q = q.order_by(desc(rcnt))
 		else: # params['sort-mode'] == "name"
 			q = q.order_by(Series.title)
-	else: # params['sort-mode'] == "name"
+	else:
 		q = q.order_by(Series.title)
 
+	q = q.limit(100)
 	res = q.all()
 
+	if res:
+		print("Results:")
+		print(res[0])
+		print(dir(res[0]))
 	return res
 
+def search_check_ok(params):
+	if (
+				'chapter-limits' in params
+			and
+				len(params['chapter-limits']) == 2
+			and
+				int(params['chapter-limits'][0]) >= 1
+		):
+		return True
+
+	if (
+				'tag-category' in params
+			and
+				len(params['tag-category']) >= 1
+		):
+		return True
+	if (
+				'series-type' in params
+			and
+				len(params['series-type']) >= 1
+		):
+		return True
+	return False
 
 @app.route('/ajax-search', methods=['POST'])
 def ajax_search():
 	print("Ajax search!")
-	series = do_advanced_search(request.json)
-	return render_template('ajax-search.html', series=series)
+	if search_check_ok(request.json):
+		series = do_advanced_search(request.json)
+		return render_template('ajax-search.html', series=series)
+	else:
+		return render_template('__block_blurb.html', message="You have to provide some search parameters!")
 
+def render_search_page(search):
+	if not search:
+
+		print("Render search page call!")
+		results = CommonTags.query                 \
+			.filter(CommonTags.tag_instances > 25) \
+			.order_by(CommonTags.tag)              \
+			.all()
+
+		return render_template('advanced-search.html',
+			available_tags = results
+			)
+	elif 'json' in request.args:
+		args = json.loads(request.args['json'])
+
+
+		if search_check_ok(args):
+			series = do_advanced_search(args)
+			return render_template('advanced-search-results.html',
+				series = series
+				)
+		else:
+			return render_template('not-implemented-yet.html', message="You have to provide some search parameters!")
 
 # @login_required
 @app.route('/search', methods=['GET', 'POST'])
 def search():
 	return execute_search()
-
-
-	# return render_template('search_results.html',
-	# 				   # sequence_item   = series_entries,
-	# 				   # page            = page,
-	# 				   name_key        = "title",
-	# 				   page_url_prefix = 'series-id',
-	# 				   searchTarget    = 'Tags',
-	# 				   # searchValue     = tag.tag
-	# 				   )
