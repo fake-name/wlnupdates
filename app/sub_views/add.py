5077185106271e3699ae96994597f6e65b07392a
Fake-Name <something@fake-url.com>
2019-02-07 21:56:03 -0800

    Something is hammering the add release form with no parameters. What?

--- app/sub_views/add.py
+++ app/sub_views/add.py
@@ -235,7 +235,7 @@ def addNewItem(add_type, sid=None):
 	else:
 		form = form_class()
 
-	print("Trying to validate:", form, request.form)
+	print("Trying to validate:", form, request.form, request.args)
 	# print(form.validate_on_submit())
 	if form.validate_on_submit():
 		print("Post request. Validating")
