87f1b723215b3aa59f7b0b548777cb7dcffcd19f
Fake-Name <something@fake-url.com>
2019-07-20 15:38:39 -0700

    Use proper flask wtf form

--- forum/forum/forms.py
+++ forum/forum/forms.py
@@ -1,4 +1,4 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import SubmitField, TextField, TextAreaField
 from wtforms.validators import Required
 from wtforms.validators import Length
@@ -6,13 +6,13 @@ from wtforms.validators import Length
 title_error_message_missing = "You have to provide a thread title!"
 title_error_message_length  = "Thread titles can only be up to 80 characters long!"
 body_error_message          = "You have to enter content for your post!"
-class CreateThreadForm(Form):
+class CreateThreadForm(FlaskForm):
 	name = TextField(validators=[Required(message=title_error_message_missing), Length(max=80, message=title_error_message_length)])
 	content = TextAreaField(validators=[Required(message=body_error_message)])
 	submit = SubmitField()
 
 
-class CreatePostForm(Form):
+class CreatePostForm(FlaskForm):
 	content = TextAreaField(validators=[Required(message=body_error_message)])
 	submit = SubmitField()
 
